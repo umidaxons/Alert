65c954907cdf0abfeef1e6fbd35104a7242e2d88
umidaxon <umidaxon@gmail.com>
2024-12-26 16:37:11 +0500

    Alert

---
+++ app.js
@@ -0,0 +1,11 @@
+//prompt
+let ism = prompt("Ismingizni kiriting:");
+
+let birinchiHarf = ism.charAt().toUpperCase();
+let davomi = ism.slice(1).toLowerCase;
+let result = birinchiHarf + davomi;
+alert("Salom, ${ism}");
+//charAt()
+//slice()
+//toUpperCase()
+//Lowercase()
